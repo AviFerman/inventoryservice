6655639af27a2b49a1b28c86afe5f8558e04325b
avife <avi.ferman@outlook.com>
2025-08-05 01:42:39 +0300

    business logic

--- src/main/java/com/orderprocessing/inventoryservice/service/KafkaService.java
+++ src/main/java/com/orderprocessing/inventoryservice/service/KafkaService.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.orderprocessing.inventoryservice.dto.OrderData;
 import com.orderprocessing.inventoryservice.dto.OrderEvent;
+import com.orderprocessing.inventoryservice.enums.ItemAvailabilityEnum;
+import com.orderprocessing.inventoryservice.enums.OrderStatusEnum;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -28,28 +30,31 @@ public class KafkaService {
             OrderData orderData = orderDataOptional.get();
             log.info("handleInventoryForOrder:: Received OrdedData: {}", orderData);
             manageOrder(orderData, orderEvent);
+//            updateOrderData(orderData);
         } catch (JsonProcessingException e) {
             log.error("Error processing inventory check message", e);
         }
     }
 
-    private static void manageOrder(OrderData orderData, OrderEvent orderEvent) {
+    private void manageOrder(OrderData orderData, OrderEvent orderEvent) {
         if (orderData != null) {
             // Here you would typically check inventory and update it accordingly
             log.info("manageOrder:: Processing inventory for Order ID: {}", orderData.getOrderId());
-            // Example: Check product availability in the catalog
-            orderData.getItems().forEach(item -> {
-                log.info("manageOrder:: Checking inventory for Item with Product ID: {}", item.getProductId());
-                // Example: Check if the item is in stock
-
-                if (item.getAvailability() == ItemAvailabilityEnum.IN_STOCK) {
-                    log.info("manageOrder:: Item {} is in stock. Quantity: {}", item.getProductId(), item.getQuantity());
-                    // Perform further processing for in-stock items
-                } else {
-                    log.warn("manageOrder:: Item {} is out of stock.", item.getProductId());
-                    // Handle out-of-stock scenario
+            if (!allPoductIdAreValid(orderData)) {
+                log.info("manageOrder:: Invalid Product IDs found in Order Data");
+                orderData.setOrderStatus(OrderStatusEnum.CANCELLED);
+                return;
                 }
-            });
+
+                orderData.getItems().forEach(item -> {
+                    log.info("manageOrder:: Checking inventory for Item with Product ID: {}", item.getProductId());
+                    if (item.getAvailability() == ItemAvailabilityEnum.IN_STOCK) {
+                        log.info("manageOrder:: Item {} is in stock. Quantity: {}", item.getProductId(), item.getQuantity());
+                    } else {
+                        log.warn("manageOrder:: Item {} is out of stock.", item.getProductId());
+                        // Handle out-f-stock scenario
+                    }
+                });
             // Simulate inventory check and update logic
             // For example, check if enough stock is available
             // If sufficient stock, proceed with order fulfillment
@@ -57,5 +62,10 @@ public class KafkaService {
         } else {
             log.warn("handleInventoryForOrder:: No OrderData found for Order ID: {}", orderEvent.getOrderId());
         }
+    }
+
+    private boolean allPoductIdAreValid(OrderData orderData) {
+        return orderData.getItems().stream()
+                .allMatch(item -> productCatalogService.getProduct(item.getProductId()) != null);
     }
 }
